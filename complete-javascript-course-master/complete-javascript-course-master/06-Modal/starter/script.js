11b119f5dc4479e6f2915a8b6c8932189923ee06
Caitlin Malloch <caitlin.malloch@gmail.com>
2022-11-15 13:09:28 -0800

    finished learning runtime

--- complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
@@ -5,13 +5,28 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+  btnsOpenModal[i].addEventListener('click', openModal);
 
-    modal.style.display = 'block';
-  });
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
