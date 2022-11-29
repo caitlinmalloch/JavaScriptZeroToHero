719631cbdf2ec2235c1a27ce0a3869b0f59b704b
Caitlin Malloch <caitlin.malloch@gmail.com>
2022-11-29 14:19:15 -0800

    finished functions unit

--- complete-javascript-course-master/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,9 +1,11 @@
 'use strict';
 
-// Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+console.log(flights.split('+'));
+
+/*
 const weekdays = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'];
 const openingHours = {
   [weekdays[3]]: {
@@ -19,6 +21,8 @@ const openingHours = {
     close: 24,
   },
 };
+
+
 
 const restaurant = {
   name: 'Classico Italiano',
@@ -49,7 +53,160 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+/*
+////////////////////////////////////////////////////////////////////////
+// SPLIT AND JOIN Strings, Capitalize
+console.log('a+very+nice+string'.split('+'));
+console.log('Caitlin Malloch'.split(' '));
 
+const [firstName, lastName] = 'Caitlin Malloch'.split(' ');
+
+const newName = ['Ms.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    namesUpper.push(n[0].toUpperCase() + n.slice(1));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('caitlin malloch');
+capitalizeName('random name');
+
+// Padding string
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '-').padEnd(30, '-'));
+
+// Hide sensitive information
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(4333333232438203480));
+console.log(maskCreditCard(795743705684760986879));
+
+// Repeat
+const message2 = 'Bad weather... All departures delayed';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(10);
+
+/*
+////////////////////////////////////////////////////////////////////
+// STRINGS PT @
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'cAiT'; // cAiT
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect); // Cait
+
+// Comparing emails
+const email = 'hello@email.io';
+const loginEmail = ' Hello@Email.Io \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+// console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// replacing
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replaceAll('door', 'gate')); // replace all instances
+
+console.log(announcement.replaceAll(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+// Practice execise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are not allowed on board');
+  } else {
+    console.log('Welcome aboard');
+  }
+};
+checkBaggage('I have a laptop, some food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+/*
+/////////////////////////////////////////////////////////////////////
+// STRINGS
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+console.log(plane[0]);
+console.log(plane[1]);
+
+console.log(airline.length);
+console.log('B737'[0]);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log(`You got the middle seat`);
+  } else {
+    console.log('You are lucky!');
+  }
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('Cait'));
+console.log(typeof new String('Cait'));
+
+console.log(typeof new String('Cait').slice(1));
+
+
+/*
 ////////////////////////////////////////////////////////////////////
 // MAPS: Iterations, similar to set method but created this way
 
--- complete-javascript-course-master/complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,355 @@
 'use strict';
+/*
+////////////////////////////////////////////////////////////
+// Default parameters
+const bookings = [];
+
+// ES6 makes it easier to set parameters
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //ES5
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 5);
+
+createBooking('LH13', undefined, 1000);
+
+
+const flight = 'LH234';
+const cait = {
+  name: 'Cait Malloch',
+  passport: 74846459834659,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Ms. Cait Malloch';
+
+  if (passenger.passport === 74846459834659) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, cait);
+// console.log(flight);
+// console.log(cait);
+
+// // Is the same as doing..
+// const flightNum = flight;
+// const passenger = cait;
+
+// Example of multiple functions calling on an object creating an error
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(cait);
+checkIn(flight, cait);
+
+//////////////////////////////////////////////////////////////
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// HIGHER ORDER FUNCTIONS
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+
+// JS uses callback functions all the time
+const high5 = function () {
+  console.log('✋');
+};
+document.body.addEventListener('click', high5);
+['Cait', 'Thomas', 'Heesook'].forEach(high5); // output: 3 highfives count because 3 elements were introduced
+
+// CALLBACK FUNCTION
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Cait');
+greeterHey('Thomas');
+
+greet('Hello')('Cait');
+
+// Challenge- simplify code
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+
+////////////////////////////////////////////////////////////
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Rachel Green');
+lufthansa.book(635, 'Monica Geller');
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// DOESNT WORK
+// book(23, 'Joey Tribbiani');
+
+//////////////////////////////////////////////////////////
+// CALL METHOD
+book.call(eurowings, 23, 'Joey Tribbiani');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Ross Geller');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Phoebe Buffet');
+console.log(swiss);
+
+/////////////////////////////////////////////////////////////
+// APPLY METHOD
+const flightData = [583, 'Chandler Bing'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+////////////////////////////////////////////////////////////
+// BIND METHOD- returns new keyword where this keyword is bound
+// book.call(eurowings, 23, 'Joey Tribbiani');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Joseph Tribbiani');
+
+// PARTIAL APPLICATION
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('George Costanza');
+
+// WITH EVENT LISTENERS
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+// lufthansa.buyPlane(); //output: 301 planes
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa)); // output: NaN
+
+// PARTIAL APPLICATION
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+*/
+
+////////////////////////////////////////////////////////////////////////
+/*
+CODING CHALLENGE 1
+Let's build a simple poll app!
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter 'poll' object below.
+
+Your tasks:
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+1.1. Display a prompt window for the user to input the number of the
+selected option. The prompt should look like this:
+What is your favourite programming language?
+0: JavaScript
+1: Python
+2: Rust
+3: C++
+(Write option number)
+1.2. Based on the input number, update the 'answers' array property. For example, if the option is 3, increase the value at position 3 of the array by
+1. Make sure to check if the input is a number and if the number makes sense (e.g. answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The
+method takes a string as an input (called 'type'), which can be either 'string'
+or 'array'. If type is 'array', simply display the results array as it is, using
+console.log(). This should be the default option. If type is 'string', display a
+string like "Poll results are 13, 2, 4, 1".
+4. Run the 'displayResults' method at the end of each
+'registerNewAnswer' method call.
+5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test
+data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll
+object! So what should the this keyword look like in this situation?
+
+Test data for bonus:
+§ Data 1: [5, 2, 3]
+§ Data 2: [1, 5, 3, 9, 6, 1]
+
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = prompt(
+      `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+    );
+    console.log(answer);
+
+    // Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      // Poll results are 13, 2, 4, 1
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+// poll.registerNewAnswer();
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+ 
+
+////////////////////////////////////////////////////////////////////
+// IIFE- IMMEDIATELY INVOKED EXPRESSION FUNCTION
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})(); // didnt need to name function by wrapping in () and () calls it immediately, var will ignore this rule
+
+// console.log(isPrivate); // cant access inner scope created by function outside
+
+(() => console.log('This will ALSO never run again'))();
+
+
+////////////////////////////////////////////////////////////////////////
+// CLOSURES
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+ */
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  //Create a timer
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
