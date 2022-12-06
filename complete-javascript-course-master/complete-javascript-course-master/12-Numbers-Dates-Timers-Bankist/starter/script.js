b19b4915c0cbb32cfd26f29e34ea2947dd7fc016
Caitlin Malloch <caitlin.malloch@gmail.com>
2022-12-06 15:43:43 -0800

    learned dates in JS

--- complete-javascript-course-master/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,208 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+/////////////////////////////////////////////////////////
+// NUMBERS
+/*
+console.log(23 === 23.0); // true
+console.log(0.1 + 0.2); // 0.30000000000000004 hahahah
+console.log(0.1 + 0.2 === 0.3); // false - error in JS
+
+// Conversion
+console.log(Number('23')); // convert string to number
+console.log(+'23'); // type coersion from string to number
+
+// Parsing
+console.log(Number.parseInt('30px')); // 30
+console.log(Number.parseInt('e23')); // NaN
+console.log(Number.parseFloat('2.5rem')); // 2.5
+
+// Check if value is Not a number
+console.log(Number.isNaN(20)); // false
+console.log(Number.isNaN('20')); // false
+console.log(Number.isNaN(+'20x')); // true
+console.log(Number.isNaN(23 / 0)); // false
+
+// Checking if value is number
+console.log(Number.isFinite(20)); // true
+console.log(Number.isFinite('20')); // false
+console.log(Number.isFinite(+'20x')); // false
+console.log(Number.isFinite(23 / 0)); // false
+
+console.log(Number.isInteger(23)); // true
+console.log(Number.isInteger(23.0)); // true
+console.log(Number.isInteger(23 / 0)); // false
+
+
+////////////////////////////////////////////
+// MATH AND ROUNDING
+console.log(Math.sqrt(25)); // 5
+console.log(25 ** (1 / 2)); // 5
+console.log(8 ** (1 / 3)); // 2
+
+console.log(Math.max(5, 18, 23, 11, 2)); // 23
+console.log(Math.max(5, 18, '23', 11, 2)); // 23
+console.log(Math.max(5, 18, '23px', 11, 2)); // NaN
+
+console.log(Math.min(5, 18, 23, 11, 2)); // 2
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2); //314.1592653589793
+
+console.log(Math.trunc(Math.random() * 6) + 1); // 2
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min; // 0...1 -> 0...(max-min) -> min...max // 2
+
+console.log(randomInt(10, 20)); // 17
+
+// Rounding integers
+console.log(Math.trunc(23.3)); // 23
+
+console.log(Math.round(23.3)); // 23
+console.log(Math.round(23.9)); // 24
+
+console.log(Math.ceil(23.3)); // 24
+console.log(Math.ceil(23.9)); // 24
+
+console.log(Math.floor(23.3)); // 23
+console.log(Math.floor('23.9')); // 23
+
+console.log(Math.trunc(23.3)); // 23
+
+console.log(Math.trunc(-23.3)); // -23
+console.log(Math.floor(-23.3)); // -24- floor works best for all situations
+
+// Floating point integers (Rounding decimals)
+console.log((2.7).toFixed(0)); // 3 // returns a string
+console.log((2.7).toFixed(3)); // 2.700, string
+console.log((2.345).toFixed(2)); // 2.35, string
+console.log(+(2.345).toFixed(2)); // 2.35, number
+
+///////////////////////////////////////////////////////////////
+// THE REMAINDER OPERATOR
+console.log(5 % 2); // 1
+console.log(5 / 2); // 2.5 (5 = 2 * 2 + 1)
+
+console.log(8 % 3); // 2
+console.log(8 / 3); // 2.66666666666665 (8 = 2 * 3 + 2)
+
+console.log(6 % 2); // 0
+console.log(6 / 2); // 3, no remainder
+
+console.log(7 % 2); // 1
+console.log(7 / 2); // 3.5 (7 = 2* 3 + 1)
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8)); // true
+console.log(isEven(23)); // false
+console.log(isEven(514)); // true
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
+// Nth
+
+/////////////////////////////////////////////////////////////
+// NUMERIC SEPERATORS
+// 287,460, 000, 000
+const diameter = 287_460_000_000; // can use _ to seperate by thousands
+console.log(diameter); // 287460000000, doesnt print seperators
+
+const price = 345_99; // _ makes code easier to read
+console.log(price); // 34599
+
+const transferFee = 15_00;
+const transferFee2 = 1_500;
+console.log(transferFee); // 1500
+
+const PI = 3.14_15;
+console.log(PI); // 3.1415, just use _ for readability
+
+console.log(Number('230000')); // 230000
+console.log(Number('230_000')); // NaN
+console.log(parseInt('230_000')); // 230, only reads until _
+
+/////////////////////////////////////////////////////////////////
+// BIGINT
+console.log(2 ** 53 - 1); // biggest number JS can store: 9007199254740991
+console.log(Number.MAX_SAFE_INTEGER); //9007199254740991
+console.log(2 ** 53 + 1); // start to lose precision, 9007199254740992
+console.log(2 ** 53 + 2); // 9007199254740994
+console.log(2 ** 53 + 3); // 9007199254740996
+console.log(2 ** 53 + 4); // 9007199254740996
+
+// Starting in ES2019, bigint solves this problem
+console.log(48326548624386846487947924797842874392); // 4.832654862438685e+37
+console.log(48326548624386846487947924797842874392n); // n transforms the number to display accurately // 48326548624386846487947924797842874392n
+
+console.log(BigInt(463284678)); // 463284678n
+
+// Operations
+console.log(10000n + 10000n); // 20000n
+console.log(48264824864832674867238476n * 39217386123681286731836721n); // 1892820272915996478850242638462082968859052600877196n
+// console.log(Math.sqrt(16n)); // error:  Cannot convert a BigInt value to a number
+
+const huge = 48732846872648732648762486734827n;
+const num = 23;
+// console.log(huge * num); // Error: cant mix bigint and other types
+console.log(huge * BigInt(num)); // 1120855478070920850921537194901021n- works
+
+// Exceptions- logical operators and string concat
+console.log(20n > 12); // true
+console.log(20n === 20); // false, no type coercion with bigint
+console.log(typeof 20n); // bigint
+console.log(20n == 20); // true, type coercion works here
+
+console.log(huge + 'is REALLY big!!!'); // 48732846872648732648762486734827is REALLY big!!!
+
+// Divisions
+console.log(11n / 3n); // 3n
+console.log(10 / 3); // 3.3333333333333
+
+
+/////////////////////////////////////////////////////////////////////
+// CREATING DATES
+const now = new Date();
+console.log(now); // Tue Dec 06 2022 15:18:06 GMT-0800 (Pacific Standard Time)
+
+console.log(new Date('Sun Aug 02 2020 18:05:41')); // Sun Aug 02 2020 18:05:41 GMT-0700 (Pacific Daylight Time)
+console.log(new Date('December 25, 2022')); // Sun Dec 25 2022 00:00:00 GMT-0800 (Pacific Standard Time)
+
+console.log(new Date(account1.movementsDates[0])); // Mon Nov 18 2019 13:31:17 GMT-0800 (Pacific Standard Time)
+
+console.log(new Date(2037, 10, 19, 15, 23, 5)); // Thu Nov 19 2037 15:23:05 GMT-0800 (Pacific Standard Time)
+
+console.log(new Date(2037, 10, 31)); // Tue Dec 01 2037 00:00:00 GMT-0800 (Pacific Standard Time)
+
+console.log(new Date(0)); /// Wed Dec 31 1969 16:00:00 GMT-0800 (Pacific Standard Time), earliest date avail on JS
+
+console.log(new Date(3 * 24 * 60 * 60 * 1000)); // Sat Jan 03 1970 16:00:00 GMT-0800 (Pacific Standard Time), 3 days later
+
+// 3 * 24 * 60 * 60 * 1000 = 259200000 = timestamp
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23); // Thu Nov 19 2037 15:23:00 GMT-0800 (Pacific Standard Time)
+console.log(future);
+console.log(future.getFullYear()); // 2037
+console.log(future.getMonth()); // 10
+console.log(future.getDate()); // 19
+console.log(future.getDay()); // 4 (4th day of week- Thursday)
+console.log(future.getHours()); // 15
+console.log(future.getMinutes()); // 23
+console.log(future.getSeconds()); // 0
+console.log(future.toISOString()); // 2037-11-19T23:23:00.000Z
+console.log(future.getTime()); // 2142285780000- the timestamp
+
+console.log(new Date(2142285780000)); //Thu Nov 19 2037 15:23:00 GMT-0800 (Pacific Standard Time)
+
+console.log(Date.now()); //1670370110081 (timestamp)
+
+future.setFullYear(2040);
+console.log(future); // Mon Nov 19 2040 15:23:00 GMT-0800 (Pacific Standard Time), year and month has changed to reflect new year/date
+
+*/
+//////////////////////////////////////////////////////////
+//
